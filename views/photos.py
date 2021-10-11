3eb0e22586b37216125d10278b2bac699608e1db
Lycaenion <linn.martensson@hotmail.se>
2021-10-11 22:12:20 +0200

    require login for photos

--- views/photos.py
+++ views/photos.py
@@ -10,10 +10,13 @@ from flask import (
     url_for
 )
 
+from .auth import login_required
+
 
 photos_blueprint = Blueprint('photos', __name__, url_prefix='/photos')
 
 
 @photos_blueprint.route('/list', methods=['GET'])
+@login_required
 def read():
     return render_template('base.html')
